c51de126155d532cdd08438373aee57f097fbfce
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2022-01-24 14:56:52 -0500

    Initial Commit

---
+++ HayleyCard/HayleyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HayleyCardApp.swift
+//  HayleyCard
+//
+//  Created by Hayley Robinson on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct HayleyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
